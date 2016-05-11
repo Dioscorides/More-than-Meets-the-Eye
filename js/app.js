d4631b3ff17afb6f1da85f9d06ad1492f1fb40af
Giulio Menna <Giulio.menna@gmail.com>
2016-05-11 13:31:34 +0200

    Now searching 2 json terms instead of one (thanks to Jan Jouke!)

--- js/app.js
+++ js/app.js
@@ -5,8 +5,8 @@ function initMap() {
 
     map = new google.maps.Map(document.getElementById('map'), {
         center: {
-            lat: 52.156944,
-            lng: 4.497010
+            lat: 52.166404,
+            lng: 4.482089
         },
         mapTypeId: google.maps.MapTypeId.SATELLITE,
         zoom: 17,
@@ -22,9 +22,9 @@ function initMap() {
 
     function placeMapper(place) {
         //Here goes the stuff for the Infowindow
-        var infowindowContent = "<h3>" + place.title + "</h3><br><p>" + place.Description + "<br/>" + '<a href=http://catalogue.leidenuniv.nl/primo_library/libweb/action/dlSearch.do?&group=GUEST&onCampus=false&vid=UBL_V1&institution=UBL&tab=special&search_scope=UBL_LMS&ct=facet&fctN=facet_rtype&fctV=Images&query=any,contains,"' + place.title + '" target="_blank">View UBL Special Collections</a>' + "</p>";
+        var infowindowContent = "<h3>" + place.title + "</h3><br><p>" + place.Description + "<br/>" + '<a href=http://catalogue.leidenuniv.nl/primo_library/libweb/action/dlSearch.do?&group=GUEST&onCampus=false&vid=UBL_V1&institution=UBL&tab=special&search_scope=UBL_LMS&ct=facet&fctN=facet_rtype&fctV=Images&query=any,contains,' + place.title.replace(" ", "%20") + ' target="_blank">View UBL Special Collections</a>' + "</p>";
         //Here goes the stuff for the Datatable
-        var row = $("<tr>" + "<td>" + place.title + "</td>" + "<td>" + place.lat + "</td>" + "<td>" + place.lon + "</td>" + "<td>" + '<a href=http://catalogue.leidenuniv.nl/primo_library/libweb/action/dlSearch.do?&group=GUEST&onCampus=false&vid=UBL_V1&institution=UBL&tab=special&search_scope=UBL_LMS&ct=facet&fctN=facet_rtype&fctV=Images&query=any,contains,"' + place.title + '" target="_blank">View UBL Special Collections</a>' + "</td>" + "</tr>");
+        var row = $("<tr>" + "<td>" + place.title + "</td>" + "<td>" + place.lat + "</td>" + "<td>" + place.lon + "</td>" + "<td>" + '<a href=http://catalogue.leidenuniv.nl/primo_library/libweb/action/dlSearch.do?&group=GUEST&onCampus=false&vid=UBL_V1&institution=UBL&tab=special&search_scope=UBL_LMS&ct=facet&fctN=facet_rtype&fctV=Images&query=any,contains,' + place.title.replace(" ", "%20") + ' target="_blank">View UBL Special Collections</a>' + "</td>" + "</tr>");
         var clickToggle = function () {
             map.setCenter({
                 lat: place.lat,
@@ -51,7 +51,7 @@ function initMap() {
     }
     $(document).ready(function () {
         $.get('js/data.json', function (tabledata) {
-              tabledata.map(placeMapper);
+            tabledata.map(placeMapper);
             //Datatable options go here!
             $('#datatablex').DataTable({
                 responsive: true,
